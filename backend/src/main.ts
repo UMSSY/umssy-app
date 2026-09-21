1d599aa36cb0c3e30faae69b52a64b0c4aa1ecf0
zer4c <pabloazurduy40@gmail.com>
2026-09-20 23:49:37 -0400

    chore: delete nginx configuration, dockerfiles.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('None')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT!);
 }
-await bootstrap();
+bootstrap();
